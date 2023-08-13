82cac6b10b289f97a39f3b64646bcc2e9178c3ce
Krishan Gupta <krishan_mu@srmuniv.edu.in>
2023-08-13 23:23:02 +0530

    Update Maximum Absolute Difference.java

--- Maximum Absolute Difference.java
+++ Maximum Absolute Difference.java
@@ -1,36 +1,36 @@
-The "Maximum Absolute Difference" problem asks you to find the maximum value of |A[i] - A[j]| + |i - j| for all possible pairs (i, j) in the given array A of integers.
+// The "Maximum Absolute Difference" problem asks you to find the maximum value of |A[i] - A[j]| + |i - j| for all possible pairs (i, j) in the given array A of integers.
 
-Here's a step-by-step explanation of the solution:
+// Here's a step-by-step explanation of the solution:
 
-Observation:
-The expression |A[i] - A[j]| + |i - j| can be split into four cases based on the values of i and j:
+// Observation:
+// The expression |A[i]-A[j]| + |i-j| can be split into four cases based on the values of i and j:
 
-Case 1: A[i] > A[j] and i > j
-Case 2: A[i] > A[j] and i < j
-Case 3: A[i] < A[j] and i > j
-Case 4: A[i] < A[j] and i < j
-Breaking Down the Cases:
+// Case 1: A [i] > A [j] and i > j
+// Case 2: A[i] > A[j] and i < j
+// Case 3: A[i] < A[j] and i > j
+// Case 4: A[i] < A[j] and i < j
+// Breaking Down the Cases:
 
-Case 1: (A[i] - A[j]) + (i - j)
-This simplifies to: (A[i] + i) - (A[j] + j)
-Case 2: (A[i] - A[j]) + (j - i)
-This simplifies to: (A[i] - i) - (A[j] - j)
-Case 3: (A[j] - A[i]) + (i - j)
-This simplifies to: (A[j] - j) - (A[i] - i)
-Case 4: (A[j] - A[i]) + (j - i)
-This simplifies to: (A[j] + j) - (A[i] + i)
-Creating Arrays:
-To efficiently calculate these values for all indices, create four arrays:
+// Case 1: (A[i] - A[j]) + (i - j)
+// This simplifies to: (A[i] + i) - (A[j] + j)
+// Case 2: (A[i] - A[j]) + (j - i)
+// This simplifies to: (A[i] - i) - (A[j] - j)
+// Case 3: (A[j] - A[i]) + (i - j)
+// This simplifies to: (A[j] - j) - (A[i] - i)
+// Case 4: (A[j] - A[i]) + (j - i)
+// This simplifies to: (A[j] + j) - (A[i] + i)
+// Creating Arrays:
+// To efficiently calculate these values for all indices, create four arrays:
 
-Array 1: Calculate the values (A[i] + i) for each index i.
-Array 2: Calculate the values (A[i]-i) for each index i.
-Array 3: Calculate the values (A[i]-i) for each index i.
-Array 4: Calculate the values (A[i] + i) for each index i.
-Finding Max Difference:
-After calculating these arrays, for each index i, you can find the maximum absolute difference by considering all four cases. The maximum value will be the answer.
+// Array 1: Calculate the values (A[i] + i) for each index i.
+// Array 2: Calculate the values (A[i]-i) for each index i.
+// Array 3: Calculate the values (A[i]-i) for each index i.
+// Array 4: Calculate the values (A[i] + i) for each index i.
+// Finding Max Difference:
+// After calculating these arrays, for each index i, you can find the maximum absolute difference by considering all four cases. The maximum value will be the answer.
 
-Implementation:
-Here's a pseudocode implementation of the solution:
+// Implementation:
+// Here's a pseudocode implementation of the solution:
 
 public class Solution {
     public int maxArr(int[] A) {
